1975c21eea10b6cb69a90e34400b7c15298b0361
gammarkin <marcosrbnsf@gmail.com>
2024-03-01 20:34:40 -0300

    fix: corrects deploy module bug

--- back/src/models/connect.js
+++ back/src/models/connect.js
@@ -1,9 +1,10 @@
+import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
 
 const POSTGRES_URL = process.env.POSTGRES_URL || '';
 const password = process.env.POSTGRES_PASSWORD || '';
 
 
-const sequelize = new Sequelize(POSTGRES_URL, { dialect: 'postgres', password })
+const sequelize = new Sequelize(POSTGRES_URL, { dialect: 'postgres', password, dialectModule: pg })
 
 export default sequelize;
