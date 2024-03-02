ccb79a3726da037b56bb31ff21df70e4293e72d4
gammarkin <marcosrbnsf@gmail.com>
2024-03-02 20:33:21 -0300

    feat: adds routes

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import App from './App.jsx';
+
+import Login from './pages/Login.jsx';
+import Products from './pages/Products.jsx';
+import GetToken from './pages/GetToken.jsx';
+import NotFound from './pages/NotFound.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/products',
+		element: <Products />,
+	},
+	{
+		path: '/get-token',
+		element: <GetToken />,
+	},
+	{
+		path: '*',
+		element: <NotFound />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
