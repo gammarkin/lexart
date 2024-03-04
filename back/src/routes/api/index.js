54a2d96b376b011116af0a30a7caf5c07e8a0b3c
gammarkin <marcosrbnsf@gmail.com>
2024-03-04 18:54:07 -0300

    feat: adds cors for all routes

--- back/src/routes/api/index.js
+++ back/src/routes/api/index.js
@@ -1,28 +1,9 @@
 import express from "express";
-import cors from "cors";
-
 import authRoutes from "./auth.js";
 import productRoutes from "./products.js";
 
-const corsOptions = {
-    origin: [
-        'https://lexart-lilac.vercel.app',
-        'https://lexart-back.vercel.app',
-    ],
-    credentials: true,
-};
-
 const router = express.Router();
 
-const conditionalCorsMiddleware = (req, res, next) => {
-    if (req.path.includes("/api")) {
-        cors(corsOptions)(req, res, next);
-    } else {
-        next();
-    }
-};
-
-router.use(conditionalCorsMiddleware);
 router.use(productRoutes, authRoutes);
 
 export default router;
